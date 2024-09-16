9275e1c79e7901826c9d9f45465eb66a5b914557
Ruentgen <ruentgen@aienergyshop.com.au>
2024-09-16 15:08:29 +0800

    update

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,30 +1,16 @@
-const path = require("path");
-
-module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT", "sqlite");
-
-  const connections = {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client: "postgres",
-      connection: {
-        host: env("DATABASE_HOST"),
-        port: env.int("DATABASE_PORT"),
-        database: env("DATABASE_NAME"),
-        user: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
-        ssl: {
-          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-        },
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
       },
-      debug: false,
     },
-  };
-
-  return {
-    connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
-    },
-  };
-};
+    debug: false,
+  },
+});
