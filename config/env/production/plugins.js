c69a58ef16e28cedc546d0d514ae255a5f673d63
Ruentgen <ruentgen@aienergyshop.com.au>
2024-09-24 09:14:29 +0800

    update

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -24,4 +24,18 @@ module.exports = () => ({
       },
     },
   },
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 10,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+        introspection: true,
+      },
+    },
+  },
 });
