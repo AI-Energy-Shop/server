448bc5e466bf72f37dd408120b8fb9a823eb642e
RuentDev <ruent.dev@gmail.com>
2025-05-26 09:12:05 +0800

    fixing upload issue

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,45 @@
 export default ({ env }) => {
   return {
+    // upload: {
+    //   config: {
+    //     provider: "aws-s3",
+    //     providerOptions: {
+    //       baseUrl: env("DO_SPACES_ENDPOINT"),
+    //       rootPath: env("DO_SPACES_ROOT_PATH"),
+    //       s3Options: {
+    //         // credentials: {
+    //         //   accessKeyId: env("DO_SPACES_ACCESS_KEY"),
+    //         //   secretAccessKey: env("DO_SPACES_SECRET_KEY"),
+    //         // },
+    //         accessKeyId: env("DO_SPACES_ACCESS_KEY"),
+    //         secretAccessKey: env("DO_SPACES_SECRET_KEY"),
+    //         region: env("DO_SPACES_REGION"),
+    //         params: {
+    //           ACL: env("AWS_ACL", "private"),
+    //           signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+    //           Bucket: env("DO_SPACES_NAME"),
+    //         },
+    //       },
+    //     },
+    //     actionOptions: {
+    //       upload: {
+    //         timeout: 60000,
+    //         sizeLimit: 10 * 1024 * 1024, // 10MB
+    //         useStream: true,
+    //         buffer: false,
+    //       },
+    //       uploadStream: {
+    //         timeout: 60000,
+    //         sizeLimit: 10 * 1024 * 1024, // 10MB
+    //         useStream: true,
+    //         buffer: false,
+    //         imageOptimization: false,
+    //       },
+    //       delete: {},
+    //     },
+    //   },
+    // },
+
     upload: {
       config: {
         provider: "aws-s3",
@@ -7,16 +47,13 @@ export default ({ env }) => {
           baseUrl: env("DO_SPACES_ENDPOINT"),
           rootPath: env("DO_SPACES_ROOT_PATH"),
           s3Options: {
-            // credentials: {
-            //   accessKeyId: env("DO_SPACES_ACCESS_KEY"),
-            //   secretAccessKey: env("DO_SPACES_SECRET_KEY"),
-            // },
-            accessKeyId: env("DO_SPACES_ACCESS_KEY"),
-            secretAccessKey: env("DO_SPACES_SECRET_KEY"),
+            credentials: {
+              accessKeyId: env("DO_SPACES_ACCESS_KEY"),
+              secretAccessKey: env("DO_SPACES_SECRET_KEY"),
+            },
+            endpoint: env("DO_SPACES_ENDPOINT"), // aws-s3 v3 needs a prefixed https:// endpoint
             region: env("DO_SPACES_REGION"),
             params: {
-              ACL: env("AWS_ACL", "public-read"),
-              signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
               Bucket: env("DO_SPACES_NAME"),
             },
           },
